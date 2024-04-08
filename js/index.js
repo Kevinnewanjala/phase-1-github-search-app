2284c325da71324592e66a682bb4278745b19a7d
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-04-08 20:51:54 +0300

    Added functionality

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const userListElement = document.getElementById('user-list');
     const reposListElement = document.getElementById('repos-list');
 
-    // Event listener for form submission
     formElement.addEventListener('submit', async (event) => {
         event.preventDefault();
         const username = document.getElementById('search').value;
@@ -12,41 +11,26 @@ document.addEventListener('DOMContentLoaded', () => {
         try {
             const userData = await fetchUserData(username);
             displayUserInfo(userData);
-            const reposData = await fetchAllUserRepositories(username);
-            displayReposCount(reposData.length); // Displaying the count of repositories
+            const reposCount = await fetchUserRepositoriesCount(username);
+            displayReposCount(reposCount);
         } catch (error) {
             console.error('Error fetching GitHub user data:', error);
         }
     });
 
-    // Fetch user data from GitHub API
     async function fetchUserData(username) {
         const url = `https://api.github.com/users/${username}`;
         const response = await fetch(url);
         return response.json();
     }
 
-    // Fetch all repositories for the user (handling pagination)
-    async function fetchAllUserRepositories(username) {
-        let allRepositories = [];
-        let page = 1;
-        let repositories = [];
-        do {
-            repositories = await fetchUserRepositories(username, page);
-            allRepositories = allRepositories.concat(repositories);
-            page++;
-        } while (repositories.length > 0);
-        return allRepositories;
-    }
-
-    // Fetch repositories from a specific page
-    async function fetchUserRepositories(username, page) {
-        const url = `https://api.github.com/users/${username}/repos?page=${page}&per_page=100`;
+    async function fetchUserRepositoriesCount(username) {
+        const url = `https://api.github.com/users/${username}`;
         const response = await fetch(url);
-        return response.json();
+        const userData = await response.json();
+        return userData.public_repos;
     }
 
-    // Display user information
     function displayUserInfo(userData) {
         userListElement.innerHTML = '';
         const userInfoItem = document.createElement('li');
@@ -54,7 +38,6 @@ document.addEventListener('DOMContentLoaded', () => {
         userListElement.appendChild(userInfoItem);
     }
 
-    // Display the count of repositories
     function displayReposCount(count) {
         reposListElement.innerHTML = '';
         const reposCountItem = document.createElement('li');
